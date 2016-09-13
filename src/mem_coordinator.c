4d63af49ca9d9f1a513c3f94f97d114059cc2129
Daniel Lobato Garcia <me@daniellobato.me>
2016-09-13 08:51:50 +0200

    Set memory collection period before collecting

--- src/mem_coordinator.c
+++ src/mem_coordinator.c
@@ -1,5 +1,6 @@
 #include <src/libvirt_domains.c>
 #include <stdio.h>
+#include <unistd.h>
 
 char * tagToMeaning(int tag) {
 	char * meaning;
@@ -51,24 +52,26 @@ void printDomainStats(struct DomainsList list)
 	printf("------------------------------------------------\n");
 	for (int i = 0; i < list.count; i++) {
 		virDomainMemoryStatStruct memstats[VIR_DOMAIN_MEMORY_STAT_NR];
-		memset(memstats,
-		       0,
-		       sizeof(virDomainMemoryStatStruct) *
-		       VIR_DOMAIN_MEMORY_STAT_NR);
-		virDomainMemoryStats(list.domains[i],
-				     (virDomainMemoryStatPtr)&memstats,
-				     VIR_DOMAIN_MEMORY_STAT_NR,
-				     0);
-		for(int j = 0; j < VIR_DOMAIN_MEMORY_STAT_NR; j++) {
-			printf("%s : %s = %lld KB\n",
+                unsigned int nr_stats;
+		unsigned int flags = VIR_DOMAIN_AFFECT_CURRENT |
+			VIR_DOMAIN_AFFECT_CONFIG |
+			VIR_DOMAIN_AFFECT_LIVE;
+                virDomainSetMemoryStatsPeriod(list.domains[i], 1, flags);
+		nr_stats = virDomainMemoryStats(list.domains[i],
+						memstats,
+						VIR_DOMAIN_MEMORY_STAT_NR,
+						0);
+		for(int j = 0; j < nr_stats; j++) {
+			printf("%s : %s = %llu MB\n",
 			       virDomainGetName(list.domains[i]),
 			       tagToMeaning(memstats[j].tag),
-			       memstats[j].val);
+			       memstats[j].val/1024);
 		}
+
 	}
 }
 
-void printHostStats(virConnectPtr conn)
+void printHostMemoryStats(virConnectPtr conn)
 {
 	int nparams = 0;
 	virNodeMemoryStatsPtr stats = malloc(sizeof(virNodeMemoryStats));
@@ -87,9 +90,28 @@ void printHostStats(virConnectPtr conn)
 	}
 	printf("Hypervisor memory: \n");
 	for (int i = 0; i < nparams; i++) {
-		printf("%8s : %lld KB\n",
+		printf("%8s : %lld MB\n",
 		       stats[i].field,
-		       stats[i].value);
+		       stats[i].value/1024);
+	}
+}
+
+long wastedMemory(virDomainPtr domain) {
+
+}
+
+virDomainPtr findWastefulDomain(struct DomainsList list) {
+	for (int i = 0; i < list.count; i++) {
+		virDomainMemoryStatStruct memstat;
+		memset (&memstat, '\0', sizeof (virDomainMemoryStatStruct));
+		virDomainMemoryStats(list.domains[i],
+				     &memstat,
+				     VIR_DOMAIN_MEMORY_STAT_NR,
+				     0);
+		printf("%s : %s = %lld KB\n",
+		       virDomainGetName(list.domains[i]),
+		       tagToMeaning(memstat.tag),
+                       memstat.val);
 	}
 }
 
@@ -118,9 +140,10 @@ int main (int argc, char **argv)
 		//   - use a threshold, e.g:
 		//     only allocate to lowest VM if 100MB away from exhaustion
 		//     otherwise free it so host can have it
-		printHostStats(conn);
+		printHostMemoryStats(conn);
+		//findWastefulDomain(list)
 		printDomainStats(list);
-		sleep(10);
+		sleep(atoi(argv[1]));
 	}
 	virConnectClose(conn);
 	return 0;
